a00f4202c9eedd294b2bca8d1ad346e277a02537
GregoryForrest <97044303+GregoryForrest@users.noreply.github.com>
2022-07-08 12:45:51 -0400

    Add in Basic Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { HashRouter, Link } from 'react-router-dom';
 import './App.css';
+import Roles from './Roles';
 import { AppHeader } from './components/AppHeader/AppHeader';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
     <AppHeader />
+    <HashRouter>
+    <Routes>
+        <Route path="roles" element={<Roles />}>
+        </Route>
+    </Routes>
+  </HashRouter>
      </div>
   );
 }
---
+++ src/Roles.tsx
@@ -0,0 +1,10 @@
+
+function Roles() {
+  return (
+    <div><h1>
+    Welcome to the Roles Admin Page
+    </h1>  </div>
+  );
+}
+
+export default Roles;
