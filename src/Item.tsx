42cc8f6a060fb9223e61cf381c859f6e48d60de3
GregoryForrest <97044303+GregoryForrest@users.noreply.github.com>
2022-07-22 14:58:54 -0400

    Use the new Request compactView

--- src/Item.tsx
+++ src/Item.tsx
@@ -1,45 +1,17 @@
-import { Button } from "@fluentui/react-components";
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { IItem, ItemApiConfig } from "./api/ItemApi";
 import { CheckList } from "./components/CheckList/CheckList";
 import { useChecklistItems } from "./api/CheckListItemApi";
+import { InForm } from "./components/InForm/InForm";
 
 export const Item: React.FunctionComponent = (props) => {
   const { itemNum } = useParams();
-  const itemApi = ItemApiConfig.getApi();
-  const [item, setItem] = useState<IItem>({ Id: 0, Title: "" });
   const checklisItems = useChecklistItems(Number(itemNum));
-
-  async function updateItem() {
-    let updatedItem = { ...item, Title: "TEST" };
-    let res = await itemApi.updateItem(updatedItem);
-    return res;
-  }
-
-  useEffect(() => {
-    const loadItem = async () => {
-      const res = await itemApi.getItemById(Number(itemNum));
-      if (res) {
-        setItem(res);
-      }
-    };
-
-    loadItem();
-  }, [itemNum]);
 
   return (
     <div>
       <h1>Welcome to the Item Page</h1>
-
-      <p>Getting info for {itemNum}</p>
-      <p>Response: {item.Title}</p>
-
+      <InForm ReqId={Number(itemNum)} compactView />
       <CheckList CheckListItems={checklisItems} />
-
-      <Button appearance="primary" onClick={updateItem}>
-        Update
-      </Button>
     </div>
   );
 };
