4eb0c394e6b38969a54dc78eacb5e18446aa5e12
GregoryForrest <97044303+GregoryForrest@users.noreply.github.com>
2022-08-23 09:05:23 -0400

    Implement React-Hook-Form

--- src/components/InRequest/InRequestEditPanel.tsx
+++ src/components/InRequest/InRequestEditPanel.tsx
@@ -1,32 +1,12 @@
 import { Panel } from "@fluentui/react";
-import { FunctionComponent, useState } from "react";
-import { useBoolean } from "@fluentui/react-hooks";
+import { FunctionComponent } from "react";
 import {
   Button,
   webLightTheme,
   FluentProvider,
 } from "@fluentui/react-components";
-import { IInForm } from "../../api/RequestApi";
-import { INFORMVIEWS, InRequest } from "./InRequest";
 
 export const InRequestEditPanel: FunctionComponent<any> = (props) => {
-  const [saveCancelEvent, setSaveCancelEvent] = useState<string>("");
-
-  /* Boolean state for determining whether or not the Edit Panel is shown */
-  const [isEditPanelOpen, { setTrue: showEditPanel, setFalse: hideEditPanel }] =
-    useBoolean(props.isEditPanelOpen);
-
-  /* Function to handle the save or cancel */
-  const onEditSaveCancel = (formEdits: IInForm | undefined): void => {
-    // Call the callback function provided as a prop to update the parent component
-    props.onEditSaveCancel(formEdits);
-
-    hideEditPanel();
-
-    // Clear the event type
-    setSaveCancelEvent("");
-  };
-
   // The footer of the EditPanel, containing the "Save" and "Cancel" buttons
   const onRenderFooterContent = () => (
     <FluentProvider theme={webLightTheme}>
@@ -34,7 +14,7 @@ export const InRequestEditPanel: FunctionComponent<any> = (props) => {
         <Button
           appearance="primary"
           onClick={() => {
-            setSaveCancelEvent("save");
+            props.onEditSaveCancel("save");
           }}
         >
           Save
@@ -42,7 +22,7 @@ export const InRequestEditPanel: FunctionComponent<any> = (props) => {
         <Button
           appearance="secondary"
           onClick={() => {
-            setSaveCancelEvent("cancel");
+            props.onEditSaveCancel("cancel");
           }}
         >
           Cancel
@@ -53,30 +33,19 @@ export const InRequestEditPanel: FunctionComponent<any> = (props) => {
 
   return (
     <>
-      {/* TODO -- Allow this to be a passed in styled component for showing/hiding the Edit panel*/}
-      <Button
-        appearance="primary"
-        className="floatRight"
-        onClick={showEditPanel}
-      >
-        Edit
-      </Button>
       <Panel
-        isOpen={isEditPanelOpen}
+        isOpen={props.isEditPanelOpen}
         isBlocking={true}
-        onDismiss={hideEditPanel}
+        onDismiss={() => {
+          props.onEditSaveCancel("cancel");
+        }}
         headerText="Edit Request"
         isFooterAtBottom={true}
         onRenderFooterContent={onRenderFooterContent}
       >
         <FluentProvider theme={webLightTheme}>
-          {/* Pass in the current formData with no ReqId so it doesn't re-read from the source */}
-          <InRequest
-            view={INFORMVIEWS.EDIT}
-            formData={props.formData}
-            onEditSaveCancel={onEditSaveCancel}
-            saveCancelEvent={saveCancelEvent}
-          />
+          {/* Pass in the form nodes */}
+          {props.children}
         </FluentProvider>
       </Panel>
     </>
