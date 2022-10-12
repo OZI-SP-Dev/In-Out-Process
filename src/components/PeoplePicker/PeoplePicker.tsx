137f8e2492be4014057c512159303b6abe6aa0ae
GregoryForrest <97044303+GregoryForrest@users.noreply.github.com>
2022-10-12 06:53:14 -0400

    Revert "Leverage predefined ENUMS in PnPJS"
    This reverts commit f02e05831d9aec273a67b9e49667dee70cd8acc9.

--- src/components/PeoplePicker/PeoplePicker.tsx
+++ src/components/PeoplePicker/PeoplePicker.tsx
@@ -8,7 +8,6 @@ import {
 import { people } from "@fluentui/example-data";
 import { spWebContext } from "../../providers/SPWebContext";
 import { IPeoplePickerEntity } from "@pnp/sp/profiles";
-import { PrincipalType, PrincipalSource } from "@pnp/sp";
 
 // TODO: Add a way to show as input needed/corrected
 
@@ -74,8 +73,8 @@ export const PeoplePicker: FunctionComponent<IPeoplePickerProps> = (props) => {
             AllowMultipleEntities: false,
             MaximumEntitySuggestions: limitResults ? limitResults : 25,
             QueryString: filterText,
-            PrincipalSource: PrincipalSource.All,
-            PrincipalType: PrincipalType.User,
+            PrincipalSource: 15,
+            PrincipalType: 1,
           });
         let newPersonas: IPersonaProps[] = [];
         results.forEach((person: IPeoplePickerEntity) => {
