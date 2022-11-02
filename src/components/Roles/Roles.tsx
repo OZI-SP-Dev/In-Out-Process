5b4e32c39dc6f61a2591af616ee14648679dcb91
GregoryForrest <97044303+GregoryForrest@users.noreply.github.com>
2022-11-02 14:09:14 -0400

    Refactor RolesApi in preparation for adding in UI for management

--- src/components/Roles/Roles.tsx
+++ src/components/Roles/Roles.tsx
@@ -1,13 +1,47 @@
 import { useContext } from "react";
-import { useAllUserRoles } from "api/RolesApi";
+import {
+  useRoleManagement,
+  RoleType,
+  useAllUserRolesByUser,
+  useAllUserRolesByRole,
+} from "api/RolesApi";
 import { UserContext } from "providers/UserProvider";
+import { Button } from "@fluentui/react-components";
 
 export const Roles: React.FunctionComponent = () => {
-  const { data: allRoles } = useAllUserRoles();
+  const { data: allRolesByUser } = useAllUserRolesByUser();
+  const { data: allRolesByType } = useAllUserRolesByRole();
   const userContext = useContext(UserContext);
+
+  // We have to turn the Map objects into Arrays to be able to read them in the JSX
+  const typeKeys = allRolesByType
+    ? Array.from(allRolesByType.keys())
+    : undefined;
+  const userKeys = allRolesByUser
+    ? Array.from(allRolesByUser.keys())
+    : undefined;
+
+  // Get the hook with functions to perform Role Management
+  const { addRole, removeRole } = useRoleManagement();
+
+  // Function to test adding a Role
+  const addRoleClick = (role: RoleType) => {
+    let user = userContext.user;
+    addRole.mutate({
+      User: user ? user : { Id: 1, EMail: "ADMIN", Title: "ADMIN" },
+      Role: role,
+    });
+  };
+
+  // Function to test removing a Role
+  const removeRoleClick = (role: number) => {
+    removeRole.mutate(role);
+  };
 
   return (
     <>
+      {/* TODO -- Replace this page with a component for Adding users -- Also with components for viewing by RoleTpye -- And Viewing by User 
+              This interface is just a placeholder for testing that RolesAPI update */}
       Current User Roles
       <ol>
         {userContext.roles?.map((role) => (
@@ -16,18 +50,43 @@ export const Roles: React.FunctionComponent = () => {
       </ol>
       All Roles
       <ol>
-        {allRoles?.map((role) => {
-          const userEntry = role.Roles?.map((roles) => {
-            return <li>{roles.Role} </li>;
-          });
-          return (
-            <>
-              <li key={role.User.Title}>{role.User.Title}</li>
-              <ol>{userEntry}</ol>
-            </>
-          );
-        })}
+        {userKeys?.map((key) => (
+          <li key={key}>
+            {allRolesByUser?.get(key)?.map((role) => role.User.Title)[0]}
+            <ol>
+              {allRolesByUser?.get(key)?.map((obj) => (
+                <li key={obj.Title}>
+                  {obj.Title}{" "}
+                  <Button onClick={() => removeRoleClick(obj.Id)}>
+                    Remove
+                  </Button>
+                </li>
+              ))}
+            </ol>
+          </li>
+        ))}
       </ol>
+      All Roles by Role
+      <ol>
+        {typeKeys?.map((key) => (
+          <li key={key}>
+            {key}
+            <ol>
+              {allRolesByType?.get(key)?.map((obj) => (
+                <li key={obj.User.Title}>
+                  {obj.User.Title}{" "}
+                  <Button onClick={() => removeRoleClick(obj.Id)}>
+                    Remove
+                  </Button>
+                </li>
+              ))}
+            </ol>
+          </li>
+        ))}
+      </ol>
+      <Button onClick={() => addRoleClick(RoleType.ATAAPS)}>Add ATAAPS</Button>
+      <Button onClick={() => addRoleClick(RoleType.ADMIN)}>Add ADMIN</Button>
+      <Button onClick={() => addRoleClick(RoleType.FOG)}>Add FOG</Button>
     </>
   );
 };
