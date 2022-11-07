af27bdff34d52fe930e2bfe980687dedcdfe3b57
GregoryForrest <97044303+GregoryForrest@users.noreply.github.com>
2022-11-07 11:46:04 -0500

    Update Roles to leverage tabs

--- src/components/Roles/Roles.tsx
+++ src/components/Roles/Roles.tsx
@@ -1,7 +1,13 @@
-import { useContext } from "react";
-import { useRoleManagement, RoleType } from "api/RolesApi";
+import { useContext, useState } from "react";
+import { RoleType } from "api/RolesApi";
 import { UserContext } from "providers/UserProvider";
-import { Button } from "@fluentui/react-components";
+import {
+  SelectTabData,
+  SelectTabEvent,
+  Tab,
+  TabList,
+  TabValue,
+} from "@fluentui/react-components";
 import { RolesByRole } from "components/Roles/RolesByRole";
 import { RolesByUser } from "components/Roles/RolesByUser";
 import { Navigate } from "react-router-dom";
@@ -9,9 +15,13 @@ import { AddUserRole } from "./AddUserRole";
 
 export const Roles: React.FunctionComponent = () => {
   const userContext = useContext(UserContext);
+  // Which tab is selected
+  const [selectedValue, setSelectedValue] = useState<TabValue>("ByRole");
 
-  // Get the hook with functions to perform Role Management
-  const { addRole } = useRoleManagement();
+  // Event when they change to "By Role" or "By User" tab
+  const onTabSelect = (event: SelectTabEvent, data: SelectTabData) => {
+    setSelectedValue(data.value);
+  };
 
   // Ensure we have a roles object before determining whether or not to redirect
   if (userContext.roles) {
@@ -21,28 +31,18 @@ export const Roles: React.FunctionComponent = () => {
     }
   }
 
-  // Function to test adding a Role
-  const addRoleClick = (role: RoleType) => {
-    let user = userContext.user;
-    addRole.mutate({
-      User: user ? user : { Id: 1, EMail: "ADMIN", Title: "ADMIN" },
-      Role: role,
-    });
-  };
-
   return (
     <>
-      {/* TODO -- Replace this page with a component for Adding users -- Also with components for viewing by RoleTpye -- And Viewing by User 
-              This interface is just a placeholder for testing that RolesAPI update */}
-      Add Role to a User
-      <AddUserRole />
-      Roles By User
-      <RolesByUser />
-      All Roles by Role
-      <RolesByRole />
-      <Button onClick={() => addRoleClick(RoleType.ATAAPS)}>Add ATAAPS</Button>
-      <Button onClick={() => addRoleClick(RoleType.ADMIN)}>Add ADMIN</Button>
-      <Button onClick={() => addRoleClick(RoleType.FOG)}>Add FOG</Button>
+      <h2>Current Assigned Roles</h2>
+      <TabList selectedValue={selectedValue} onTabSelect={onTabSelect}>
+        <Tab value="ByRole">By Role</Tab>
+        <Tab value="ByUser">By User</Tab>
+      </TabList>
+      {selectedValue === "ByRole" && <RolesByRole />}
+      {selectedValue === "ByUser" && <RolesByUser />}
+      <hr />
+      <h2>Add User to Role</h2>
+      <AddUserRole />{" "}
     </>
   );
 };
