9cc8f8fc2d7ef6548a9c4f51c17d9ec72adc8509
GregoryForrest <97044303+GregoryForrest@users.noreply.github.com>
2022-11-08 07:11:41 -0500

    Leverage CommandBar with multiple delete for Roles

--- src/components/Roles/RolesByRole.tsx
+++ src/components/Roles/RolesByRole.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {
   useRoleManagement,
   RoleType,
-  SPRole,
   useAllUserRolesByRole,
+  SPRole,
 } from "api/RolesApi";
 import {
   makeStyles,
@@ -18,8 +18,10 @@ import {
   DetailsList,
   IColumn,
   SelectionMode,
+  Selection,
+  CommandBar,
+  ICommandBarItemProps,
 } from "@fluentui/react";
-import { CancelIcon } from "@fluentui/react-icons-mdl2";
 
 /* FluentUI Styling */
 const useStyles = makeStyles({
@@ -41,19 +43,8 @@ export const RolesByRole: React.FunctionComponent = () => {
   const classes = useStyles();
   const { data: allRolesByType } = useAllUserRolesByRole();
   const [selectedValue, setSelectedValue] = useState<TabValue>(RoleType.ADMIN);
-  const initialItems = allRolesByType?.get(RoleType.ADMIN);
-  const [items, setItems] = useState<SPRole[]>(
-    initialItems ? initialItems : []
-  );
-
-  useEffect(() => {
-    const newItems = allRolesByType?.get(selectedValue as RoleType);
-    if (newItems === undefined) {
-      setItems([]);
-    } else {
-      setItems(newItems);
-    }
-  }, [allRolesByType, selectedValue]);
+  const items = allRolesByType?.get(selectedValue as RoleType) || [];
+  const [selection] = useState(new Selection());
 
   // We don't want to show SUPERVISOR or EMPLOYEE roles as something to view
   const rolesToShow = Object.values(RoleType)
@@ -72,15 +63,20 @@ export const RolesByRole: React.FunctionComponent = () => {
       isResizable: true,
       onRender: (item) => item.User.Title,
     },
+  ];
+
+  const commandItems: ICommandBarItemProps[] = [
     {
-      key: "action",
-      name: "",
-      minWidth: 20,
-      maxWidth: 20,
-      isResizable: false,
-      onRender: (item) => (
-        <CancelIcon onClick={() => removeRoleClick(item.Id)} />
-      ),
+      key: "delete",
+      text: "Delete",
+      iconProps: { iconName: "Delete" },
+      onClick: () => {
+        let selectedEntries = selection.getSelection();
+        for (let entry of selectedEntries) {
+          let spRoleEntry = entry as SPRole;
+          removeRole.mutate(spRoleEntry.Id);
+        }
+      },
     },
   ];
 
@@ -90,11 +86,6 @@ export const RolesByRole: React.FunctionComponent = () => {
 
   // Get the hook with functions to perform Role Management
   const { removeRole } = useRoleManagement();
-
-  // Function to test removing a Role
-  const removeRoleClick = (role: number) => {
-    removeRole.mutate(role);
-  };
 
   return (
     <>
@@ -113,11 +104,13 @@ export const RolesByRole: React.FunctionComponent = () => {
           </TabList>
         </div>
         <div className={classes.userList}>
+          <CommandBar items={commandItems}></CommandBar>
           <DetailsList
             items={items}
             columns={columns}
-            selectionMode={SelectionMode.none}
+            selectionMode={SelectionMode.multiple}
             constrainMode={ConstrainMode.unconstrained}
+            selection={selection}
           ></DetailsList>
           {items.length === 0 && <>No users with this role</>}
         </div>
--- src/components/Roles/RolesByUser.tsx
+++ src/components/Roles/RolesByUser.tsx
@@ -7,8 +7,11 @@ import {
   DetailsList,
   IColumn,
   SelectionMode,
+  Selection,
+  CommandBar,
+  ICommandBarItemProps,
 } from "@fluentui/react";
-import { CancelIcon, ContactIcon, ListIcon } from "@fluentui/react-icons-mdl2";
+import { ContactIcon } from "@fluentui/react-icons-mdl2";
 import { Label, makeStyles, Text } from "@fluentui/react-components";
 
 /* FluentUI Styling */
@@ -36,6 +39,7 @@ export const RolesByUser: React.FunctionComponent = () => {
   const { data: allRolesByUser } = useAllUserRolesByUser();
   const [items, setItems] = useState<SPRole[]>([]);
   const [selectedUser, setSelectedUser] = useState<IPerson>();
+  const [selection] = useState(new Selection());
 
   useEffect(() => {
     if (typeof selectedUser?.EMail === "string") {
@@ -52,31 +56,31 @@ export const RolesByUser: React.FunctionComponent = () => {
 
   const columns: IColumn[] = [
     {
-      key: "name",
+      key: "role",
       name: "Role",
       minWidth: 100,
       isResizable: true,
       onRender: (item) => item.Title,
-    },
-    {
-      key: "action",
-      name: "",
-      minWidth: 20,
-      maxWidth: 20,
-      isResizable: false,
-      onRender: (item) => (
-        <CancelIcon onClick={() => removeRoleClick(item.Id)} />
-      ),
     },
   ];
 
   // Get the hook with functions to perform Role Management
   const { removeRole } = useRoleManagement();
 
-  // Function to test removing a Role
-  const removeRoleClick = (role: number) => {
-    removeRole.mutate(role);
-  };
+  const commandItems: ICommandBarItemProps[] = [
+    {
+      key: "delete",
+      text: "Delete",
+      iconProps: { iconName: "Delete" },
+      onClick: () => {
+        let selectedEntries = selection.getSelection();
+        for (let entry of selectedEntries) {
+          let spRoleEntry = entry as SPRole;
+          removeRole.mutate(spRoleEntry.Id);
+        }
+      },
+    },
+  ];
 
   return (
     <div className={classes.formContainer}>
@@ -96,16 +100,13 @@ export const RolesByUser: React.FunctionComponent = () => {
         ></PeoplePicker>
       </div>
       <div className={classes.fieldContainer}>
-        <Label size="small" weight="semibold" className={classes.fieldLabel}>
-          <ListIcon className={classes.fieldIcon} />
-          Roles
-        </Label>
+        <CommandBar items={commandItems}></CommandBar>
         <DetailsList
           items={items}
           columns={columns}
-          selectionMode={SelectionMode.none}
+          selectionMode={SelectionMode.multiple}
           constrainMode={ConstrainMode.unconstrained}
-          isHeaderVisible={false}
+          selection={selection}
         ></DetailsList>
         {items.length === 0 && (
           <Text>No roles assigned to the selected user.</Text>
