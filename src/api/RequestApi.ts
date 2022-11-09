162abfa1085c36a4f63df228382022dacc9c2bfc
RobertPorterfield <57455266+RobertPorterfield@users.noreply.github.com>
2022-11-09 15:59:53 -0500

    merge fixes

--- src/api/RequestApi.ts
+++ src/api/RequestApi.ts
@@ -130,25 +130,14 @@ const getMyRequests = async () => {
   if (userId === undefined) {
     return Promise.reject([]);
   } else {
-      return spWebContext.web.lists
-        .getByTitle("Items")
-        .items.filter(
-          `(supGovLead/Id eq '${userId}' or employee/Id eq '${userId}') and closedOrCancelledDate eq null`
-        )
-        .select(requestedFields)
-        .expand(expandedFields)();
-    }
-  }
-};
-
-const getRequest = async (Id: number) => {
     return spWebContext.web.lists
       .getByTitle("Items")
       .items.filter(
-        `supGovLead/Id eq '${userId}' or employee/Id eq '${userId}'`
+        `(supGovLead/Id eq '${userId}' or employee/Id eq '${userId}') and closedOrCancelledDate eq null`
       )
       .select(requestedFields)
       .expand(expandedFields)();
+  }
 };
 
 const getRequest = async (Id: number) => {
